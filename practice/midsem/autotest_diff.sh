2f35f7615b30f9f9f2ced63de8874c989785bb96
braedonww@gmail.com <braedonww@gmail.com>
2019-10-08 22:12:09 +1100

    Updated shell scripts to be nicer

--- practice/midsem/autotest_diff.sh
+++ practice/midsem/autotest_diff.sh
@@ -6,9 +6,9 @@ make
 for test in difference_tests/*; do
   ./difference < $test
   if [ $? -ne 0 ]; then
-    echo -e "Test $test \e[31mError\e[39m"
+    printf "Test $test \e[31mError\e[39m\n"
     exit 1
   else
-    echo -e "Test $test \e[32mSuccess\e[39m"
+    printf "Test $test \e[32mSuccess\e[39m\n"
   fi
 done
--- practice/midsem/autotest_poly.sh
+++ practice/midsem/autotest_poly.sh
@@ -6,9 +6,12 @@ make
 for test in poly_tests/*; do
   ./polyfit < $test
   if [ $? -ne 0 ]; then
-    echo -e "Test $test \e[31mError\e[39m"
+    printf "Test $test \e[31mError\e[39m\n"
     exit 1
   else
-    echo -e "Test $test \e[32mSuccess\e[39m"
+    printf "Test $test \e[32mSuccess\e[39m\n"
   fi
 done
+
+
+
