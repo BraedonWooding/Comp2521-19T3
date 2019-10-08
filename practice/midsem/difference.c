e0bfdbc0894c43803f0a3f4a1919f3508926cd21
Braedon Wooding <b.wooding@student.unsw.edu.au>
2019-10-08 15:30:41 +1100

    Midsem is up now properly

--- practice/midsem/difference.c
+++ practice/midsem/difference.c
@@ -1,48 +1,5 @@
 #include "list.h"
 
-void insertSorted(List list, int elem) {
-    // insert in sorted order
-    if (list->first == NULL) {
-        Node new = malloc(sizeof *new);
-        new->value = elem;
-        list->first = list->last = new;
-        list->size++;
-        return;
-    } else if (list->first->value > elem) {
-        Node new = malloc(sizeof *new);
-        new->value = elem;
-        new->next = list->first;
-        list->first = list->first->prev = new;
-        list->size++;
-        return;
-    }
-
-    for (Node cur = list->first; cur != NULL; cur = cur->next) {
-        if (cur->value < elem && (cur->next == NULL || cur->next->value > elem)) {
-            Node new = malloc(sizeof *new);
-            new->value = elem;
-            new->next = cur->next;
-            new->prev = cur;
-            cur->next = new;
-            if (cur->next == NULL) {
-                list->last = new;
-            } else {
-                cur->next->prev = new;
-            }
-        }
-    }
-    list->size++;
-}
-
-int contains(List list, int elem) {
-    for (Node cur = list->first; cur != NULL; cur = cur->next) {
-        if (cur->value == elem) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
 /*
     The difference between the two lists i.e. difference of [1, 2, 3] and [4, 2]
     is going to be [1, 3, 4] (note: sorted)
@@ -52,17 +9,5 @@ int contains(List list, int elem) {
     The list should be a new list
 */
 List difference(List a, List b) {
-    List res = newList();
-    for (Node cur = a->first; cur != NULL; cur = cur->next) {
-        if (!contains(b, cur->value)) {
-            insertSorted(res, cur->value);
-        }
-    }
-    for (Node cur = b->first; cur != NULL; cur = cur->next) {
-        if (!contains(a, cur->value)) {
-            insertSorted(res, cur->value);
-        }
-    }
-
-    return res;
+    return NULL;
 }
